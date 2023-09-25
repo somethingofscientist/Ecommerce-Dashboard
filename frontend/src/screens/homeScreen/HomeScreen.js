b7a5ed2fc1aae3037178e640032da3a116f04121
somethingofscientist <1shraddhanand@gmail.com>
2023-09-25 11:12:17 +0530

    create table with styles - [sahil]

--- frontend/src/screens/homeScreen/HomeScreen.js
+++ frontend/src/screens/homeScreen/HomeScreen.js
@@ -2,28 +2,58 @@ import React, { useState, useEffect } from 'react';
 import styles from './HomeScreen.module.css';
 import DataTable from 'react-data-table-component';
 import axios from 'axios';
+import { AiFillDelete } from 'react-icons/ai'
+import { FiEdit2 } from 'react-icons/fi'
 
-const columns = [
-  {
-    name: 'Product Name',
-    selector: (row) => row.name,
-  },
-  {
-    name: 'Product Price',
-    selector: (row) => row.price,
-  },
-  {
-    name: 'Company',
-    selector: (row) => row.company,
-  },
-  {
-    name: 'Category',
-    selector: (row) => row.category,
-  },
-];
+
 
 export const HomeScreen = () => {
   const [productData, setProductData] = useState([]);
+
+  const handleEdit = () => { }
+  const handleDelete = () => { }
+
+  const columns = [
+    {
+      name: 'Product Name',
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: 'Product Price',
+      selector: (row) => row.price,
+      sortable: true,
+    },
+    {
+      name: 'Company',
+      selector: (row) => row.company,
+      sortable: true,
+    },
+    {
+      name: 'Category',
+      selector: (row) => row.category,
+      sortable: true,
+    },
+    {
+      name: 'Actions',
+      cell: (row) => (
+        <div
+          className={styles.table_icon_holder}
+        >
+          <div
+            className={styles.table_icon}
+            onClick={() => handleEdit(row)}>
+            <FiEdit2 />
+          </div>
+          <div
+            className={styles.table_icon}
+            onClick={() => handleDelete(row)}>
+            <AiFillDelete />
+          </div>
+        </div>
+      ),
+    },
+  ];
 
   useEffect(() => {
     axios.get('http://localhost:9000/allProducts')
@@ -38,12 +68,24 @@ export const HomeScreen = () => {
 
   return (
     <div className={styles.homeScreen}>
-      <h1>HomeScreen Here ...</h1>
-      <DataTable
-        columns={columns}
-        data={productData}
-        pagination
-      />
+      {/* <h1>HomeScreen Here ...</h1> */}
+      <div className={styles.table}>
+        <DataTable
+          title="Product List"
+          columns={columns}
+          data={productData}
+          pagination
+
+          // selectableRows
+          // dense
+          // customStyles={customStyles}
+
+          highlightOnHover
+          pointerOnHover
+          fixedHeader
+          fixedHeaderScrollHeight="1000px"
+        />
+      </div>
     </div>
   );
 };
